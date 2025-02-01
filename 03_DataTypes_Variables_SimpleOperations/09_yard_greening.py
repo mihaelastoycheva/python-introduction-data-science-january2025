92c460c229a5fce84409f52c8c436d61f0916b01
mihaelastoycheva <msdesign.business@gmail.com>
2025-02-01 19:21:12 +0200

    added last exercise

--- 03_DataTypes_Variables_SimpleOperations/09_yard_greening.py
+++ 03_DataTypes_Variables_SimpleOperations/09_yard_greening.py
@@ -6,5 +6,5 @@ price_with_discount = cost_of_landscaping * 0.18
 
 final_price = cost_of_landscaping - price_with_discount
 
-print(f"The final price is: {final_price} lv.\n"
+print(f"The final price is: {final_price} lv.\n" 
       f"The discount is: {price_with_discount} lv.")
